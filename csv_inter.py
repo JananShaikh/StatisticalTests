a9670d50a0936852d8aece10199f122db2044c30
Mosamat Sabiha Shaikh <shkmos004@myuct.ac.za>
2023-08-23 23:33:39 +0200

    Inter-transcriber similarity code added

--- csv_inter.py
+++ csv_inter.py
@@ -1,24 +1,60 @@
-import csv
+import pandas as pd
+import sklearn
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-def read_csv_and_store(filename):
-    data_list = []
+def read_csv_and_store_lines(csv_file):
+    data = []
+    
+    # Read the CSV file into a DataFrame
+    df = pd.read_csv(csv_file)
+    
+    # Convert each row of the DataFrame into a dictionary and store in the 'data' list
+    for index, row in df.iterrows():
+        data_item = {
+            "HITId": row["HITId"],
+            "HITTypeId": row["HITTypeId"],
+            "Title": row["Title"],
+            "CreationTime": row["CreationTime"],
+            "MaxAssignments": row["MaxAssignments"],
+            "AssignmentDurationInSeconds": row["AssignmentDurationInSeconds"],
+            "AssignmentId": row["AssignmentId"],
+            "WorkerId": row["WorkerId"],
+            "AcceptTime": row["AcceptTime"],
+            "SubmitTime": row["SubmitTime"],
+            "WorkTimeInSeconds": row["WorkTimeInSeconds"],
+            "Input.audio_url": row["Input.audio_url"],
+            "Answer.transcript": row["Answer.transcript"],
+            "Turkle.Username": row["Turkle.Username"]
+        }
+        data.append(data_item)
+    
+    return data
 
-    with open(filename, 'r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        for row in csv_reader:
-            data_list.append(row)
-
-    return data_list
-
-def main():
-    csv_filename = 'Data/Unstructured1.csv'  # Replace with the actual filename
-
-    try:
-        stored_data = read_csv_and_store(csv_filename)
-        for entry in stored_data:
-            print(entry)  # You can modify this to store the data in any desired format
-    except FileNotFoundError:
-        print(f"File '{csv_filename}' not found.")
+def calculate_similarity(transcripts):
+    tfidf_vectorizer = TfidfVectorizer()
+    tfidf_matrix = tfidf_vectorizer.fit_transform(transcripts)
+    similarity_matrix = cosine_similarity(tfidf_matrix)
+    return similarity_matrix
 
 if __name__ == "__main__":
-    main()
+    csv_file_path = "Data/Structured1.csv"  # Replace with the path to your CSV file
+    stored_data = read_csv_and_store_lines(csv_file_path)
+    
+    audio_url_to_transcripts = {}
+    
+    for entry in stored_data:
+        audio_url = entry["Input.audio_url"]
+        transcript = entry["Answer.transcript"]
+        
+        if audio_url not in audio_url_to_transcripts:
+            audio_url_to_transcripts[audio_url] = []
+        audio_url_to_transcripts[audio_url].append(transcript)
+    
+    for audio_url, transcripts in audio_url_to_transcripts.items():
+        similarity_matrix = calculate_similarity(transcripts)
+        print(f"Audio URL: {audio_url}")
+        print("Similarity Matrix:")
+        print(similarity_matrix)
+        print("=" * 30)
+
