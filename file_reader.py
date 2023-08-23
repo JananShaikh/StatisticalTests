bd169f8092e98c5de88fbf406b73f6158a65d4fc
Mosamat Sabiha Shaikh <shkmos004@myuct.ac.za>
2023-08-23 20:54:08 +0200

    Corpus creation feature added

--- file_reader.py
+++ file_reader.py
@@ -1,4 +1,12 @@
 import pandas as pd
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+# Download the 'punkt' resource
+nltk.download('punkt')
+nltk.download('stopwords')
 
 def read_csv_and_store_lines(csv_file):
     data = []
@@ -28,6 +36,31 @@ def read_csv_and_store_lines(csv_file):
     
     return data
 
+def preprocess_text(text):
+    # Tokenize the text
+    tokens = word_tokenize(text)
+    
+    # Convert to lowercase
+    tokens = [token.lower() for token in tokens]
+    
+    # Remove punctuation and stopwords
+    stop_words = set(stopwords.words('english'))
+    tokens = [token for token in tokens if token.isalnum() and token not in stop_words]
+    
+    # Apply stemming
+    stemmer = PorterStemmer()
+    tokens = [stemmer.stem(token) for token in tokens]
+    
+    return tokens
+
+def create_corpus(data):
+    corpus = []
+    for entry in data:
+        transcript = entry["Answer.transcript"]
+        preprocessed_transcript = preprocess_text(transcript)
+        corpus.extend(preprocessed_transcript)
+    return corpus
+
 if __name__ == "__main__":
     csv_file_path = "Data/Structured2.csv"  # Replace with the path to your CSV file
     stored_data = read_csv_and_store_lines(csv_file_path)
@@ -35,10 +68,14 @@ if __name__ == "__main__":
     #for entry in stored_data:
     #    print(entry)
 
-    for entry in stored_data:
-        #print("HIT ID:", entry["HITId"])
-        print("User:", entry["Turkle.Username"])
-        print("Audio URL:", entry["Input.audio_url"])
-        print("Transcript:", entry["Answer.transcript"])
-        print("=" * 30)  # Separator for better readability
+    #for entry in stored_data:
+    #    #print("HIT ID:", entry["HITId"])
+    #    print("User:", entry["Turkle.Username"])
+    #    print("Audio URL:", entry["Input.audio_url"])
+    #    print("Transcript:", entry["Answer.transcript"])
+    #    print("=" * 30)  # Separator for better readability
+        
+    corpus = create_corpus(stored_data)
+    print("Corpus:", corpus)
+    print("Corpus Size:", len(corpus))    
 
